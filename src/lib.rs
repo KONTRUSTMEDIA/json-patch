cbd64df380e0c7fa4c8c22739ae707017736e67b
vovacooper <vovacooper@gmail.com>
2024-11-13 19:47:36 +0700

    bump version to 2.0.4 in Cargo.toml and update device structure in merge_ktm_tests.json

--- src/lib.rs
+++ src/lib.rs
@@ -686,63 +686,33 @@ pub fn merge(doc: &mut Value, patch: &Value, original_doc: &Value) {
     }
 }
 
-/// Patch provided JSON document (given as `serde_json::Value`) in place with JSON Merge Patch
-/// (RFC 7396).
-///
-/// # Example
-/// Create and patch document:
-///
-/// ```rust
-/// #[macro_use]
-/// use json_patch::merge;
-/// use serde_json::json;
-///
-/// # pub fn main() {
-/// let mut doc = json!({
-///   "title": "Goodbye!",
-///   "author" : {
-///     "givenName" : "John",
-///     "familyName" : "Doe"
-///   },
-///   "tags":[ "example", "sample" ],
-///   "content": "This will be unchanged"
-/// });
-/// let original_doc = doc.clone();
-///
-/// let patch = json!({
-///   "title": "Hello!",
-///   "phoneNumber": "+01-123-456-7890",
-///   "author": {
-///     "familyName": null
-///   },
-///   "tags": [ "example" ]
-/// });
-///
-/// merge(&mut doc, &patch, &original_doc);
-/// assert_eq!(doc, json!({
-///   "title": "Hello!",
-///   "author" : {
-///     "givenName" : "John"
-///   },
-///   "tags": [ "example" ],
-///   "content": "This will be unchanged",
-///   "phoneNumber": "+01-123-456-7890"
-/// }));
-/// # }
-/// ```
+/// merge RTB Patch
 pub fn merge_rtb(
-    doc: &mut Value,
+    bidrequest: &mut Value,
     patch: &Value,
-    original_doc: &Value,
+    bidrequest_original: &Value,
     supply_partner: &str,
     seller_id: Option<&str>,
     seller_domain: Option<&str>,
     seller_tag_id: Option<&str>,
 ) {
-    // if patch.is_array() {
-    //     *doc = patch.clone();
-    //     return;
-    // }
+    let new_patch = eval_patch(
+        patch.clone(),
+        bidrequest_original,
+        supply_partner,
+        seller_id,
+        seller_domain,
+        seller_tag_id,
+    );
+
+    // println!("--- patch: {:#?} -> {:#?}", patch, new_patch);
+    inner_merge(bidrequest, &new_patch, bidrequest_original);
+}
+
+/// Patch provided JSON document (given as `serde_json::Value`) in place with RTB Patch
+fn inner_merge(doc: &mut Value, patch: &Value, original_doc: &Value) {
+    let mut patch = patch.clone();
+
     if !patch.is_object() {
         *doc = patch.clone();
         return;
@@ -751,162 +721,22 @@ pub fn merge_rtb(
         *doc = Value::Object(Map::new());
     }
 
+    let patch = patch.as_object_mut().unwrap();
     let map = doc.as_object_mut().unwrap();
-    for (key, value) in patch.as_object().unwrap() {
-        let mut value = value.clone();
+    for (key, value) in patch.iter_mut() {
+        // let mut value = value.clone();
 
         // in special case where key == imps (modify all imp vector)
         if key == "imps" {
             let impression_vector = map.get_mut("imp");
-
-            // println!("impression_vector: {:#?}", impression_vector);
             // let impression_vector = doc.pointer_mut("/imp");
             match impression_vector {
                 Some(impression_vector) => {
                     for imp in impression_vector.as_array_mut().unwrap() {
-                        merge_rtb(
-                            imp,
-                            &value,
-                            original_doc,
-                            supply_partner,
-                            seller_id,
-                            seller_domain,
-                            seller_tag_id,
-                        );
+                        inner_merge(imp, &value, original_doc);
                     }
                 }
                 None => continue,
-            }
-            continue;
-        }
-        // special case where key starts with __bidrequest
-        if value.is_string() && value.as_str().unwrap().starts_with("__bidrequest") {
-            let doc_path = value
-                .as_str()
-                .unwrap()
-                .strip_prefix("__bidrequest")
-                .unwrap();
-            let doc_value = original_doc.pointer(doc_path).cloned();
-            match doc_value {
-                Some(doc_value) => {
-                    // *(doc.pointer_mut(&format!("/{}", key)).unwrap()) = doc_value.clone();
-                    map[key.as_str()] = doc_value.clone();
-                }
-                None => continue,
-            }
-            continue;
-        }
-        if key == "pchain" && value.is_string() && value.as_str().unwrap().starts_with("generate") {
-            if seller_tag_id.is_none() || seller_id.is_none() {
-                // if fore some reason there is no seller_id or _seller_idseller_domain, set it to null
-                value = json!(null);
-            } else {
-                value = format!("{}:{}", seller_tag_id.unwrap(), seller_id.unwrap()).into();
-            }
-        }
-        if key == "schain" && value.is_string() && value.as_str().unwrap().starts_with("generate") {
-            if seller_domain.is_none() || seller_id.is_none() {
-                // if fore some reason there is no seller_id or seller_domain, set it to null
-                value = json!(null);
-            } else {
-                let rid = original_doc.pointer("/id").cloned();
-                value = json!({
-                    "ver": "1.0",
-                    "nodes": [
-                        {
-                            "hp": 1,
-                            "asi": seller_domain.unwrap(),
-                            "sid": seller_id.unwrap(),
-                            "rid": rid.unwrap(),
-                        }
-                    ],
-                    "complete": 1
-                });
-            }
-        }
-
-        // search for a special type object
-        // {
-        //    "_type": "map",
-        //    "key": "__bidrequest/app/bundle/id", // will evaluate the __bidrequest/app/bundle/id passed and return map[${__bidrequest.app.bundle.id}]
-        //    "key": "supply_source", // will evaluate the supply_source passed and return map[${supply_source}]
-        //    "map": {
-        //        "best_app1": "1",
-        //        "best_app2": "2",
-        //    }
-        // }
-        // will replace object with map[bidrequest[key]]
-        if value.is_object() && value["_type"] == json!("map") {
-            let map_key = &value["key"];
-            let map_table = &value["map"];
-
-            // check key is string
-            if !map_key.is_string() {
-                // did not found the key in the map. return None
-                println!("[ERROR] key is not string!!!");
-                continue;
-            }
-
-            // println!("map_key: {:#?}", map_key);
-            // println!("map: {:#?}", map_table);
-            if map_key.as_str().unwrap().to_ascii_lowercase() == "supply_partner" {
-                // println!("supply_partner: [{:#?}]", supply_partner);
-                // let supply_partner = match supply_partner {
-                //     Some(sp) => {
-                //         // *(doc.pointer_mut(&format!("/{}", key)).unwrap()) = v.clone();
-                //         map_table.get(supply_partner.unwrap())
-                //     }
-                //     None => {
-                //         map.remove(key.as_str());
-                //         continue;
-                //     }
-                // };
-
-                // println!("new_val: {:#?}", new_val);
-                // println!("key: {:#?}", key);
-                // println!("doc: {:#?}", map);
-                match map_table.get(supply_partner) {
-                    Some(sid) => {
-                        // *(doc.pointer_mut(&format!("/{}", key)).unwrap()) = v.clone();
-                        // println!("sid: {:#?}", sid);
-                        map.insert(key.to_string(), sid.clone());
-                    }
-                    None => {
-                        map.remove(key.as_str());
-                    }
-                }
-            }
-
-            if map_key.as_str().unwrap().starts_with("__bidrequest") {
-                let doc_path = map_key
-                    .as_str()
-                    .unwrap()
-                    .strip_prefix("__bidrequest")
-                    .unwrap();
-                let mapped_key = original_doc.pointer(doc_path).cloned();
-                // println!("mapped_key: {:#?}", mapped_key);
-                if mapped_key.is_none() {
-                    // did not found the key in the map. return None
-                    // println!("key is not string or does not start with __bidrequest");
-                    map.remove(key.as_str());
-                    continue;
-                }
-                let mapped_key = mapped_key.unwrap();
-                if mapped_key.is_string() {
-                    let new_val = map_table.get(mapped_key.as_str().unwrap());
-                    // println!("new_val: {:#?}", new_val);
-                    // println!("key: {:#?}", key);
-                    // println!("doc: {:#?}", map);
-                    match new_val {
-                        Some(v) => {
-                            // *(doc.pointer_mut(&format!("/{}", key)).unwrap()) = v.clone();
-                            map.insert(key.to_string(), v.clone());
-                        }
-                        None => {
-                            map.remove(key.as_str());
-                        }
-                    }
-                }
             }
             continue;
         }
@@ -915,15 +745,193 @@ pub fn merge_rtb(
         if value.is_null() {
             map.remove(key.as_str());
         } else {
-            merge_rtb(
+            inner_merge(
                 map.entry(key.as_str()).or_insert(Value::Null),
                 &value,
                 original_doc,
+            );
+        }
+    }
+}
+
+/// eval patch object
+///
+/// # Examples
+/// in:
+/// {
+///    "_type": "map",
+///    "key": "__bidrequest/app/bundle/id", // will evaluate the __bidrequest/app/bundle/id passed and return map[${__bidrequest.app.bundle.id}]
+///    "map": {
+///        "best_app1": "1",
+///        "best_app2": "2",
+///    }
+/// }
+/// result: map[bidrequest.app.bundle.id]
+///
+/// in:
+/// {
+///    "_type": "map",
+///    "key": "supply_partner",
+///    "map": {
+///        "best_app1": "1",
+///        "best_app2": "2",
+///    }
+/// }
+/// result: map[supply_partner]
+///
+/// in:
+/// {
+///     "key": "__bidrequest/app/bundle/id"
+/// }
+/// result
+/// {
+///     "key": bidrequest.app.bundle.id"
+/// }
+///
+/// in:
+/// {
+///     "key": "__bidrequest/not/existing/key"
+/// }
+/// result
+/// {
+///     "key": null
+/// }
+fn eval_patch(
+    patch: Value,
+    bidrequest: &Value,
+    supply_partner: &str,
+    seller_id: Option<&str>,
+    seller_domain: Option<&str>,
+    seller_tag_id: Option<&str>,
+) -> Value {
+    // special case where key starts with __bidrequest
+    if patch.is_string() && patch.as_str().unwrap().starts_with("__bidrequest") {
+        let doc_path = patch
+            .as_str()
+            .unwrap()
+            .strip_prefix("__bidrequest")
+            .unwrap();
+        let doc_value = bidrequest.pointer(doc_path).cloned();
+        match doc_value {
+            Some(doc_value) => {
+                // *(doc.pointer_mut(&format!("/{}", key)).unwrap()) = doc_value.clone();
+                return doc_value.clone();
+            }
+            None => {
+                return Value::Null;
+            }
+        }
+    } else if patch.is_string() && patch.as_str().unwrap().starts_with("__supply_partner") {
+        return supply_partner.into();
+    } else if patch.is_string() && patch.as_str().unwrap().starts_with("__generate_pchain") {
+        if seller_tag_id.is_none() || seller_id.is_none() {
+            // if fore some reason there is no seller_id or _seller_idseller_domain, set it to null
+            return json!(null);
+        } else {
+            return format!("{}:{}", seller_tag_id.unwrap(), seller_id.unwrap()).into();
+        }
+    } else if patch.is_string() && patch.as_str().unwrap().starts_with("__generate_schain") {
+        if seller_domain.is_none() || seller_id.is_none() {
+            // if fore some reason there is no seller_id or seller_domain, set it to null
+            return json!(null);
+        } else {
+            let rid = bidrequest.pointer("/id").cloned();
+            return json!({
+                "ver": "1.0",
+                "nodes": [
+                    {
+                        "hp": 1,
+                        "asi": seller_domain.unwrap(),
+                        "sid": seller_id.unwrap(),
+                        "rid": rid.unwrap(),
+                    }
+                ],
+                "complete": 1
+            });
+        }
+    }
+    // spatial object "map"
+    else if patch.is_object() && patch["_type"] == json!("map") {
+        // map object
+        let map_key = &patch["key"];
+        let map_table = &patch["map"];
+
+        // check key is string
+        if !map_key.is_string() {
+            // did not found the key in the map. return None
+            // println!("[ERROR] key is not string!!!");
+            return Value::Null;
+        }
+
+        let map_key = map_key.as_str().unwrap();
+
+        if map_key.starts_with("__bidrequest") {
+            let doc_path = map_key.strip_prefix("__bidrequest").unwrap();
+            let mapped_key = bidrequest.pointer(doc_path).cloned();
+            if mapped_key.is_none() {
+                return Value::Null;
+            }
+            let mapped_key = mapped_key.unwrap();
+            // println!("3 vovacooper mapped_key: {:#?}", mapped_key);
+
+            if mapped_key.is_string() {
+                let new_val = map_table.get(mapped_key.as_str().unwrap()).cloned();
+                // println!("4 vovacooper new val: {:#?}", new_val);
+                match new_val {
+                    Some(new_val) => {
+                        return new_val;
+                    }
+                    None => {
+                        // println!("[ERROR] mapped_key is not string!!! unsupported!");
+                        return Value::Null;
+                    }
+                }
+            } else {
+                println!("[ERROR] mapped_key is not string!!! unsupported!");
+                return Value::Null;
+            }
+        } else if map_key.to_ascii_lowercase() == "supply_partner" {
+            let res = map_table.get(supply_partner).cloned();
+            // println!("vovacooper: supply_partner = {:#?}, {:#?}", supply_partner, res);
+            match res {
+                Some(res) => {
+                    return res;
+                }
+                None => {
+                    return Value::Null;
+                }
+            }
+        };
+    } else if patch.is_array() {
+        let mut array = Vec::new();
+        let arr = patch.as_array().unwrap();
+        for val in arr.iter() {
+            let ep = eval_patch(
+                val.clone(),
+                bidrequest,
                 supply_partner,
                 seller_id,
                 seller_domain,
                 seller_tag_id,
             );
+            array.push(ep);
         }
+        return Value::Array(array);
+    } else if patch.is_object() {
+        let mut new_map = Map::new();
+        let map = patch.as_object().unwrap();
+        for (key, value) in map.iter() {
+            let ep = eval_patch(
+                value.clone(),
+                bidrequest,
+                supply_partner,
+                seller_id,
+                seller_domain,
+                seller_tag_id,
+            );
+            new_map.insert(key.clone(), ep);
+        }
+        return Value::Object(new_map.clone());
     }
+    return patch.clone();
 }
